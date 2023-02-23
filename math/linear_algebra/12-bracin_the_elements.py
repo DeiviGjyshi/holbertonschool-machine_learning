c5d89d82d27334a58af4cd671852ecd82eb97156
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-02-23 06:25:16 -0800

    New

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -3,7 +3,7 @@
 
 
 def np_elementwise(mat1, mat2):
-    add = np.add(mat1, mat2)
+    add = numpy.add(mat1, mat2)
     sub = np.subtract(mat1, mat2)
     mul = np.multiply(mat1, mat2)
     div = np.divide(mat1, mat2)
