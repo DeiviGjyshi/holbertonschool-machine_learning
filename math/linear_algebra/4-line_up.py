bf3205ec8714d74c341b68d76897fa8b46943de9
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-02-23 05:42:17 -0800

    correction

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -5,7 +5,7 @@
 def add_arrays(arr1, arr2):
     """Line up"""
     if len(arr1) == len(arr2):
-        res=[arr1[x] + arr2[x] for x in range (len(arr1))]
+        res = [arr1[x] + arr2[x] for x in range(len(arr1))]
         return res
     else:
         return None
