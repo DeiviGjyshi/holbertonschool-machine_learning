f3a797cedf1619b331707c66b35abd8b47c51428
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-02-23 02:56:16 -0800

    Adding task 2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 """Size me please"""
 
-
 def matrix_shape(matrix):
     """Size me please"""
-    mat = []
-    mat.append(len(matrix))
-    if type(matrix[0]) == list:
-        mat.append(len(matrix[0]))
-        if type(matrix[0][0]) == list:
-            mat.append(len(matrix[0][0]))
-    return mat
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
