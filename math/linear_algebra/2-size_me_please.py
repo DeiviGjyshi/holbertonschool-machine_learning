978f387bd03a869f86af947da6e95733b48aa63c
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-02-23 02:38:40 -0800

    Adding test 2 changed

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 def matrix_shape(matrix):
     mat = []
     mat.append(len(matrix))
@@ -7,9 +7,3 @@ def matrix_shape(matrix):
         if type(matrix[0][0]) == list:
             mat.append(len(matrix[0][0]))
     return mat
-                
-mat1 = [[1, 2], [3, 4]]
-print(matrix_shape(mat1))
-mat2 = [[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]],
-        [[16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]]
-print(matrix_shape(mat2))
