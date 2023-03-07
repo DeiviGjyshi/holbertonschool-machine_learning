50ec1916ed03efeba1dbd1410d49a67f629d096e
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-03-07 08:05:12 -0800

    new

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -6,9 +6,13 @@ def poly_integral(poly, C=0):
     """Task 17 integrate"""
     if not isinstance(C, int) or not isinstance(poly, list) or len(poly) == 0:
         return None
-    k = len(poly)
-    res = []
-    res.append(C)
-    for j in range(k):
-       res.append(poly[j] / (j + 1) if poly[j] % (j + 1) != 0 else int(poly[j] / (j + 1)))
-    return res
+    integral = [C]
+    for power, coefficient in enumerate(poly):
+        if (coefficient % (power + 1)) == 0:
+            new_coefficient = coefficient // (power + 1)
+        else:
+            new_coefficient = coefficient / (power + 1)
+        integral.append(new_coefficient)
+    while integral[-1] == 0 and len(integral) > 1:
+        integral = integral[:-1]
+    return integral
