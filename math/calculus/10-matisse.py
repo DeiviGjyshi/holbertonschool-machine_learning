84ada7c6e0a6410682e10528fc91c21a86edea68
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-03-07 07:53:07 -0800

    new

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,15 +4,18 @@
 
 def poly_derivative(poly):
     """Task 10 matisse"""
-    if not isinstance(poly ,list):
+    if type(poly) is not list or len(poly) < 1:
         return None
-    n = len(poly)
-    res = []
-    for j in range(n):
-        if j == 0:
+    for coefficient in poly:
+        if type(coefficient) is not int and type(coefficient) is not float:
+            return None
+    for power, coefficient in enumerate(poly):
+        if power == 0:
+            derivative = [0]
             continue
-        elif j == 1:
-            res.append(poly[j])
-        elif j > 1:
-            res.append(poly[j] * j)
-    return res
+        if power == 1:
+            derivative = []
+        derivative.append(power * coefficient)
+    while derivative[-1] == 0 and len(derivative) > 1:
+        derivative = derivative[:-1]
+    return derivative
