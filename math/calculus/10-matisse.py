ebeb78191b0cb3ac1dbea70ed243725d6ac1e626
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-03-07 07:41:06 -0800

    new

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Task 10 matisse"""
+
+
+def poly_derivative(poly):
+    """Task 10 matisse"""
+    if len(poly) <= 1 or not isinstance(poly,list):
+        return None
+    n = len(poly)
+    res = []
+    for j in range(n):
+        if j == 0:
+            continue 
+        elif j == 1:
+            res.append(poly[j])
+        elif j > 1:
+            res.append(poly[j] * j)
+    return res
