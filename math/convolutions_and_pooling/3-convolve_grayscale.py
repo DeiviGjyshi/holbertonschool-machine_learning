450154353e1dd4129907169d4f0283358c76cfae
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-03-18 07:17:36 -0700

    new

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -15,12 +15,12 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
         ph = pw = 0
     else:
         ph, pw = padding
-    images_padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
+    images_p = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
     out_h = int((h + 2 * ph - kh) / sh + 1)
     out_w = int((w + 2 * pw - kw) / sw + 1)
     output_4 = np.zeros((m, out_h, out_w))
     for i in range(out_h):
         for j in range(out_w):
-            var = images_padded[:, i*sh:i*sh+kh, j*sw:j*sw+kw]
+            var = images_p[:, i*sh:i*sh+kh, j*sw:j*sw+kw]
             output_4[:, i, j] = np.sum(var * kernel, axis=(1, 2))
     return output_4
