9ec84ec058a3e60d3ec19bde5ec5a046bd94ee1b
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-03-18 07:32:49 -0700

    new

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+    """Convolution with channels"""
     m, h, w, c = images.shape
     kh, kw, _ = kernel.shape
     sh, sw = stride
@@ -14,12 +15,12 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
         ph = pw = 0
     else:
         ph, pw = padding
-    images_p = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
+    im_p = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
     out_h = int((h + 2 * ph - kh) / sh + 1)
     out_w = int((w + 2 * pw - kw) / sw + 1)
     out = np.zeros((m, out_h, out_w))
     for i in range(out_h):
         for j in range(out_w):
-            window = images_p[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
+            window = im_p[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :]
             out[:, i, j] = np.sum(window * kernel, axis=(1, 2, 3))
     return out
