374c835d8a273ea3954215e498ac609274b695bf
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-03-18 06:31:10 -0700

    new

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -7,17 +7,17 @@ def convolve_grayscale_same(images, kernel):
     """Convolution same"""
     m, h, w = images.shape
     kh, kw = kernel.shape
+    ph = max(int((kh - 1) / 2), 0)
+    pw = max(int((kw - 1) / 2), 0)
+    padded_im = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
     convoluted = np.zeros((m, h, w))
     for i in range(h - kh + 1):
         for j in range(w - kw + 1):
             if kh % 2 == 0 or kw % 2 == 0:
                 output = np.sum(images[:, i:i+kh, j:j+kw] * kernel,
-                            axis=1).sum(axis=1)
+                                axis=1).sum(axis=1)
             else:
-                ph = max(int((kh - 1) / 2), 0)
-                pw = max(int((kw - 1) / 2), 0)
-                padded_im = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
                 output = np.sum(padded_im[:, i:i+kh, j:j+kw] * kernel,
-                            axis=1).sum(axis=1)
+                                axis=1).sum(axis=1)
             convoluted[:, i, j] = output
     return convoluted
