18a39d1275e7491b3b97d64ab5b1c96b5f625de5
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-03-28 14:08:52 -0700

    new

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -5,9 +5,9 @@ import numpy as np
 
 def mean_cov(X):
     """Mean and covariance"""
-    n, d = X.shape
     if type(X) is not np.ndarray or len(X.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
+    n, d = X.shape
     if n < 2:
         raise ValueError("X must contain multiple data points")
     mean = np.mean(X, axis=0)
