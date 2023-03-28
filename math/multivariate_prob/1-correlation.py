59becdd42b64d45cb9eddd6569977a091f1d86fb
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-03-28 14:14:15 -0700

    new

---
+++ math/multivariate_prob/1-correlation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""calculates a correlation matrix"""
+import numpy as np
+
+
+def correlation(C):
+   """calculates a correlation matrix"""
+   if type(C) is not np.ndarray:
+      raise TypeError("C must be a numpy.ndarray")
+   d = C.shape[0]
+   if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
+      raise ValueError("C must be a 2D square matrix")
+   corr = np.zeros((d, d))
+   for i in range(d):
+      for j in range(d):
+         corr[i,j] = C[i,j] / np.sqrt(C[i,i] * C[j,j])
+   return corr
