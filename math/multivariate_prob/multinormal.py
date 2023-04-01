cbec556a70c3b6060aa148776a69195fd6e3c3ba
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-04-01 07:15:28 -0700

    new

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -13,7 +13,6 @@ class MultiNormal:
         if n < 2:
             raise ValueError("data must contain multiple data points")
         self.mean, self.cov = self.mean_cov(data.T)
-        self.mean = self.mean.reshape(-1, 1)
 
     def mean_cov(self, X):
         """Mean and convolution"""
