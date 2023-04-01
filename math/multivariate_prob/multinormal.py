7da8edce9348f74054608521d56201b5efeb1592
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-04-01 07:12:42 -0700

    new

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -28,3 +28,21 @@ class MultiNormal:
             cov += np.outer((X[i] - mean), (X[i] - mean))
         cov /= (n - 1)
         return mean.reshape(1, d), cov
+    
+    def pdf(self, x):
+        """return the pdf"""
+        if type(x) is not np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.cov.shape[0]
+        if len(x.shape) != 2:
+            raise ValueError("x must have the shape ({}, 1)".format(d))       
+        test_d, one = x.shape
+        if test_d != d or one != 1:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        pdf = 1.0 / np.sqrt(((2 * np.pi) ** d) * det)
+        mult = np.matmul(np.matmul((x - self.mean).T, inv), (x - self.mean))
+        pdf *= np.exp(-0.5 * mult)
+        pdf = pdf[0][0]
+        return pdf
