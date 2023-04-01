0dd8d55e8f4d0b46ba07ff3d1c6091342f2de96b
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-04-01 06:38:49 -0700

    new

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -4,17 +4,25 @@ import numpy as np
 
 
 class MultiNormal:
-  """Multivariate normal"""
-   def __init__(self, data):
-    """Multivariate normal"""
-     if type(data) is not np.ndarray or len(data.shape) != 2:
-         raise TypeError("data must be a 2D numpy.ndarray")
-     n, d = data.shape
-     if n < 2:
-         raise ValueError("data must contain multiple data points")
-     self.mean = np.mean(data, axis=0, keepdims=True).T
-     self.cov = np.zeros((d, d))
-     for i in range(n):
-         x = data[i, :].reshape((-1, 1))
-         self.cov += (x - self.mean) @ (x - self.mean).T
-     self.cov /= (n - 1)
+    """Multinormal probability class"""
+    def __init__(self, data):
+        """Multinormal init function"""
+        n, d = data.shape
+        if type(data) is not np.ndarray or len(data.shape) != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+        if n < 2:
+            raise ValueError("data must contain multiple data points")
+        self.mean, self.cov = mean_cov(data.T)
+        self.mean = self.mean.reshape(-1, 1)    
+    def mean_cov(X):
+        n, d = X.shape
+        if type(X) is not np.ndarray or len(X.shape) != 2:
+            raise TypeError("X must be a 2D numpy.ndarray")
+        if n < 2:
+            raise ValueError("X must contain multiple data points")
+        mean = np.mean(X, axis=0)
+        cov = np.zeros((d, d))
+        for i in range(n):
+            cov += np.outer((X[i] - mean), (X[i] - mean))
+        cov /= (n - 1)
+        return mean.reshape(1, d), cov
