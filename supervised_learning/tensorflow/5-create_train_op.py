0637d1a6b324320d3f49781ba5884ca3d63723f4
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-04-25 05:31:02 -0700

    new

---
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Create the training operation for the neural network
+"""
+import tensorflow as tf
+
+
+def create_train_op(loss, alpha):
+    """
+    Create the training operation for the network
+    """
+    gradient_descent = tf.train.GradientDescentOptimizer(alpha)
+    return (gradient_descent.minimize(loss))
