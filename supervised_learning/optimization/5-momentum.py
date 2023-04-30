d52f515ea66c2670e601ddb2534460cb6bf9b8ed
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-04-30 04:23:03 -0700

    new

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -5,6 +5,6 @@ import numpy as np
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
     """Update momentum"""
-    vt = np.multiply(beta1, v) + np.multiply((1- beta1), grad)
+    vt = np.multiply(beta1, v) + np.multiply((1 - beta1), grad)
     var = var - np.multiply(alpha, vt)
     return (var, vt)
