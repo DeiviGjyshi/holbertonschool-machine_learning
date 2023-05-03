c3cc1bd04bffb1f9e97d00cd94d3b93ad2732a6b
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-03 06:16:27 -0700

    new

---
+++ supervised_learning/optimization/14-batch_norm.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Optimization task"""
+import tensorflow as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    """Batch normalization upgraded"""
+    W = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    model = tf.layers.Dense(units=n, name="layer", kernel_initializer=W)
+    X = model(prev)
+    beta = tf.Variable(tf.constant(0.0, shape=[n]), trainable=True)
+    gamma = tf.Variable(tf.constant(1.0, shape=[n]), trainable=True)
+    mean, variance = tf.nn.moments(X, [0])
+    norm = tf.nn.batch_normalization(X, mean, variance, offset=beta,
+                                     scale=gamma, variance_epsilon=1e-8)
+    return activation(norm)
