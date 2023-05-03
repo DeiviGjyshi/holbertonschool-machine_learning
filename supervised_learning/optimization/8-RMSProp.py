1982b1a23c884f2f8c0ac4e28a9b414b740bebba
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-03 01:41:59 -0700

    new

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
     """RMS prop upgraded"""
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon)
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2,
+                epsilon=epsilon)
     train_op = optimizer.minimize(loss)
     return train_op
