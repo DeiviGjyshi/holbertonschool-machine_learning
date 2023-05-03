abdd299da07b01bde7ce079fc57b9021a17404f4
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-03 01:48:14 -0700

    mnew

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
     """Adam upgraded"""
-    optimizer = tf.keras.optimizers.Adam(
+    optimizer = tf.train.optimizers.Adam(
         learning_rate=alpha,
         beta_1=beta1,
         beta_2=beta2,
