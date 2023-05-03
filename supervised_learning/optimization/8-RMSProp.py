ca58847c8f36f473ef424fb23dc57d3bb6a444b6
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-03 01:40:18 -0700

    new

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Optimization task"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """RMS prop upgraded"""
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon)
+    train_op = optimizer.minimize(loss)
+    return train_op
