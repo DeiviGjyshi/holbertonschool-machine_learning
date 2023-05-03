d649db20959b0e1bb2089dcd7a311c65f5d0b0c9
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-03 01:53:49 -0700

    new

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -4,10 +4,8 @@ import tensorflow as tf
 
 
 def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
-    """Adam upgraded"""
-    optimizer = tf.train.AdamOptimizer(
-        alpha,
-        beta_1=beta1,
-        beta_2=beta2,
-        epsilon=epsilon)
-    return optimizer.minimize(loss)
+    """that creates the training operation for a neural network
+    in tensorflow using the Adam optimization algorithm"""
+    optimizer = tf.train.AdamOptimizer(alpha, beta1=beta1, beta2=beta2,
+                                       epsilon=epsilon)
+    return (optimizer.minimize(loss))
