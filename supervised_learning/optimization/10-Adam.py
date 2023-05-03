f2a6c0c4e8a97db304c226291a557e2367bc876c
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-03 01:45:55 -0700

    new

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Optimization task"""
+import tensorflow as tf
+
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """Adam upgraded"""
+    optimizer = tf.keras.optimizers.Adam(
+        learning_rate=alpha,
+        beta_1=beta1,
+        beta_2=beta2,
+        epsilon=epsilon)
+    return optimizer.minimize(loss)
