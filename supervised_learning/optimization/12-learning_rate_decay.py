4ad49676768f3ef36cac985f5a3957f4f68a35a1
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-03 02:09:55 -0700

    new

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -5,6 +5,5 @@ import tensorflow as tf
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """Learning rate decay upgraded"""
-    alpha_new = tf.divide(alpha, tf.pow(1 + decay_rate * 
-                          tf.floor_div(global_step, decay_step), 1))
-    return alpha_new
+    return tf.train.inverse_time_decay(alpha, global_step, decay_step,
+                                       decay_rate, staircase=True)
