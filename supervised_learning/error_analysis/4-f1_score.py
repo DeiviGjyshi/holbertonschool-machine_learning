ce9a45d859800e93984199b08f3c6065ce07ef9a
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-07 07:17:19 -0700

    new

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Error analysis task"""
 import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
     """F1 score"""
-    f1 = (2 * precision * sensitivity) / (precision + sensitivity)
-    return f1
+    v = (sensitivity(confusion) ** -1) + (precision(confusion) ** -1)
+    return (2 / v)
