9e00c6be78c8f7419d97bf59e604509a68addf57
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-10 07:15:25 -0700

    new

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -7,6 +7,6 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     """L2 regularization"""
     w = 0
     for i in range(1, L + 1):
-        w =w + np.linalg.norm(weights['W' + str(i)])
+        w = w + np.linalg.norm(weights['W' + str(i)])
     L2cost = cost + ((lambtha / (2 * m)) * w)
     return L2cost
