62ea33c9db758b472c0eb00bb23c7132a8595ebd
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-12 05:46:05 -0700

    new

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -4,17 +4,17 @@ import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-        """L2 Gradient descent"""
-        A2 = cache["A{}".format(L)]
-        dz = A2 - Y
-        for i in range(L, 0, -1):
-            L2_reg = (lambtha / Y.shape[1]) * weights["W{}".format(i)]
-            db = (np.sum(dz, axis=1, keepdims=True) / Y.shape[1])
-            dw = (np.matmul(dz, cache["A{}".format(i - 1)].T) / Y.shape[1]) + L2_reg
-            dz = np.matmul(weights["W" + str(
-                i)].T, dz) * (1 - (cache["A" + str(
-                    i - 1)] ** 2))
-            weights["b{}".format(i)] = weights["b{}".format(
-                i)] - (alpha * db)
-            weights["W{}".format(i)] = weights["W{}".format(
-                i)] - (alpha * dw)
+    """L2 Gradient descent"""
+    A2 = cache["A{}".format(L)]
+    dz = A2 - Y
+    for i in range(L, 0, -1):
+        L2_reg = (lambtha / Y.shape[1]) * weights["W{}".format(i)]
+        db = (np.sum(dz, axis=1, keepdims=True) / Y.shape[1])
+        dw = (np.matmul(dz, cache["A{}".format(i - 1)].T) / Y.shape[1]) + L2_reg
+        dz = np.matmul(weights["W" + str(
+            i)].T, dz) * (1 - (cache["A" + str(
+                i - 1)] ** 2))
+        weights["b{}".format(i)] = weights["b{}".format(
+            i)] - (alpha * db)
+        weights["W{}".format(i)] = weights["W{}".format(
+            i)] - (alpha * dw)
