e7176e9c3f23883f180f28e2bd02d030bcf7d4ce
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-13 08:23:36 -0700

    new

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -13,7 +13,7 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
         if (i - 1) > 0:
             dz = np.matmul(weights["W{}".format(
                 i)].T, dz) * (1 - cache["A{}".format(i - 1)]
-                                * cache["A{}".format(i - 1)])
+                              * cache["A{}".format(i - 1)])
             dz = dz * cache["D{}".format(i - 1)]
             dz = dz / keep_prob
         weights["b{}".format(i)] = weights["b{}".format(
