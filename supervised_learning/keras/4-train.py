2886363808afcc102367855e46d91450796d17c0
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-18 08:26:46 -0700

    new

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -7,7 +7,7 @@ def train_model(network, data, labels, batch_size, epochs,
                 verbose=True, shuffle=False):
     """Train keras model"""
     history = network.fit(data, labels, 
-                          batch_size=batch_size, 
-                          epochs=epochs, verbose=verbose, 
-                          shuffle=shuffle)
+                          batch_size = batch_size, 
+                          epochs = epochs, verbose = verbose, 
+                          shuffle = shuffle)
     return history
