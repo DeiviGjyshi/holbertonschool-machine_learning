52d441d0ec3a7b0615212ce7cd4910269ceef264
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-18 08:33:38 -0700

    new

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Keras task"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,`
+                validation_data=None, verbose=True, shuffle=False):
+    """Train keras model"""
+    history = network.fit(data, labels,
+                          batch_size=batch_size,
+                          epochs=epochs, verbose=verbose,
+                          validation_data=validation_data,
+                          shuffle=shuffle)
+    return history
