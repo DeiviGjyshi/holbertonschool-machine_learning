75b8dd2f20e71f79ecadf4534e0783a09c852f84
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-18 08:29:28 -0700

    new

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -6,8 +6,8 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size, epochs,
                 verbose=True, shuffle=False):
     """Train keras model"""
-    history = network.fit(data, labels, 
-                          batch_size=batch_size, 
-                          epochs=epochs, verbose=verbose, 
+    history = network.fit(data, labels,
+                          batch_size=batch_size,
+                          epochs=epochs, verbose=verbose,
                           shuffle=shuffle)
     return history
