8501bad0bce8992ea656621c7faab33b7b176528
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-19 13:56:53 -0700

    new

--- supervised_learning/keras/11-config.py
+++ supervised_learning/keras/11-config.py
@@ -5,11 +5,15 @@ import tensorflow.keras as K
 
 def save_config(network, filename):
     """Save model config"""
-    network.model.to_json(filename)
+    model = network.model.to_json(filename)
+    with open(filename, "w") as f:
+        f.write(model)
     return None
 
 
 def load_config(filename):
     """Load model config"""
-    K.models.model_from_json(filename)
-    return None
+    with open(filename, "w") as f:
+        model = f.read()
+    network = K.models.model_from_json(model)
+    return network
