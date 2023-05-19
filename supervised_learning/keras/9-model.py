d8ffd6144d1798ff02b6aa7f8650e82e3f63816c
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-19 07:23:36 -0700

    new

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Keras task"""
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """Save model"""
+    network.save(filename)
+    return None
+
+def load_model(filename):
+    """Load model"""
+    model = K.Models.load_models(filename)
+    return model
