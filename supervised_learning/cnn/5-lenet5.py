d36d0279ff6ed53ae0923e6d048edebd71bb361c
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-05-29 00:54:58 -0700

    new

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -12,7 +12,7 @@ def lenet5(X):
     conv2 = K.layers.Conv2D(16, kernel_size=(5, 5), padding='valid',
                             kernel_initializer='he_normal',
                             activation='relu')(maxpool1)
-    maxpool2 = K.layers.MaxPool2D((2,2), (2, 2))(conv2)
+    maxpool2 = K.layers.MaxPool2D((2, 2), (2, 2))(conv2)
     flatten = K.layers.Flatten()(maxpool2)
     dense1 = K.layers.Dense(units=120, kernel_initializer='he_normal',
                             activation='relu')(flatten)
