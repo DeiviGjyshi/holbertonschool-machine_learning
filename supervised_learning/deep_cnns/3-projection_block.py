43248730537d4eeaee0796ef8079516bf0b97466
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-06-03 10:41:44 -0700

    new

---
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Deep neural architecture"""
+import tensorflow.keras as K
+
+
+def projection_block(A_prev, filters, s=2):
+    """Projection block"""
+    F11, F3, F12 = filters
+    c1 = K.layers.Conv2D(filters=F11,
+                         kernel_size=(1, 1),
+                         padding='same',
+                         strides=(s, s),
+                         kernel_initializer='he_normal')(A_prev)
+    c1 = K.layers.BatchNormalization(axis=3)(c1)
+    c1 = K.layers.Activation('relu')(c1)
+    c1 = K.layers.Conv2D(filters=F3,
+                         kernel_size=(3, 3),
+                         padding='same',
+                         kernel_initializer='he_normal')(c1)
+    c1 = K.layers.BatchNormalization(axis=3)(c1)
+    c1 = K.layers.Activation('relu')(c1)
+    c1 = K.layers.Conv2D(filters=F12,
+                         kernel_size=(1, 1),
+                         padding='same',
+                         kernel_initializer='he_normal')(c1)
+    c1 = K.layers.BatchNormalization(axis=3)(c1)
+    c2 = K.layers.Conv2D(filters=F12,
+                         kernel_size=(1, 1),
+                         padding='same',
+                         strides=(s, s),
+                         kernel_initializer='he_normal')(A_prev)
+    c2 = K.layers.BatchNormalization(axis=3)(c2)
+    c3 = K.layers.Add()([c1, c2])
+    c3 = K.layers.Activation('relu')(c3)
+    return c3
