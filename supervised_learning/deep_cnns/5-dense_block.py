b277a008524746e9eacf7fc0a5569a0ec91f5fa3
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-06-04 13:49:55 -0700

    new

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -10,17 +10,17 @@ def dense_block(X, nb_filters, growth_rate, layers):
         L = K.layers.BatchNormalization(axis=3)(prev)
         L = K.layers.Activation('relu')(L)
         L = K.layers.Conv2D(filters=growth_rate * 4,
-                         kernel_size=(1, 1),
-                         padding='same',
-                         strides=(1, 1),
-                         kernel_initializer='he_normal')(L)
+                            kernel_size=(1, 1),
+                            padding='same',
+                            strides=(1, 1),
+                            kernel_initializer='he_normal')(L)
         L = K.layers.BatchNormalization(axis=3)(L)
         L = K.layers.Activation('relu')(L)
         L = K.layers.Conv2D(filters=growth_rate,
-                         kernel_size=(3, 3),
-                         padding='same',
-                         strides=(1, 1),
-                         kernel_initializer='he_normal')(L)
+                            kernel_size=(3, 3),
+                            padding='same',
+                            strides=(1, 1),
+                            kernel_initializer='he_normal')(L)
         prev = K.layers.concatenate([prev, L], axis=3)
         nb_filters = nb_filters + growth_rate
     return(prev, nb_filters)
