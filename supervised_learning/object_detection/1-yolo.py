26beb04b3c604e5619e84ec38c6ca67ef84725a3
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-06-22 08:01:09 -0700

    new

---
+++ supervised_learning/object_detection/1-yolo.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Object detection"""
+import tensorflow.keras as K
+
+
+class Yolo:
+    """Yolo class"""
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """Class constructor"""
+        with open(classes_path) as f:
+            lines = f.readlines()
+        classes = [x.strip() for x in lines]
+        self.model = K.models.load_model(model_path)
+        self.class_names = classes
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
+    def process_outputs(self, outputs, image_size):
+        """process outputs"""
+        boxes = []
+        box_confidences = []
+        box_class_probs = []
+        for o in range(len(outputs)):
+            boxes.append(outputs[o][..., :4])
+            box_confidences.append(1 / (1 + np.exp(-outputs[o][..., 4:5])))
+            box_class_probs.append(1 / (1 + np.exp(-outputs[o][..., 5:])))
+        image_height, image_width = image_size
+        for i in range(len(boxes)):
+            grid_width  = outputs[i].shape[1]
+            grid_height = outputs[i].shape[0]
+            anchor_boxes = outputs[i].shape[2]
+            for cy in range(grid_height):
+                for cx in range(grid_width):
+                    for b in range(anchor_boxes):
+                        tx, ty, tw, th = boxes[i][cy, cx, b]
+                        pw, ph = self.anchors[i][b]
+                        bx = (1 / (1 - np.exp(-tx))) + cx
+                        by = (1 / (1 - np.exp(-ty))) + cy
+                        bw = pw * np.exp(tw)
+                        bh = ph * np.exp(th)
+                        bx /= grid_width
+                        by /= grid_height
+                        bw /= self.model.input.shape[1]
+                        bh /= self.model.input.shape[2]
+                        x1 = (bx - bw / 2) * image_width
+                        y1 = (by - bh / 2) * image_height
+                        x2 = (bx - bw / 2) * image_width
+                        y2 = (by - bh / 2) * image_height
+                        boxes[i][cy, cx, b] = [x1, y1, x2, y2]
+        return(boxes, box_confidences, box_class_probs)
