a9b3aa50a1a27990e6eec35315c552ed4edfc0d3
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-09-20 10:32:27 -0700

    new

---
+++ unsupervised_learning/clustering/5-pdf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Clusterin tasks"""
+import numpy as np
+
+
+def pdf(X, m, S):
+    """PDF calculation"""
+    if type(X) is not np.ndarray or type(m) is not np.ndarray:
+        return None
+    if type(S) is not np.ndarray:
+        return None
+    if len(X.shape) != 2 or len(S.shape) != 2:
+        return None
+    if len(m.shape) != 1:
+        return None
+    n, d = X.shape
+    if m.shape[0] != d or S.shape[0] != d or S.shape[1] != d:
+        return None
+    S_det = np.linalg.det(S)
+    S_inv = np.linalg.inv(S)
+    N = np.sqrt((2*np.pi)**d * S_det)
+    fac = np.einsum('...k,kl,...l->...', X-m, S_inv, X-m)
+    P = np.exp(-fac / 2) / N
+    P[np.where(P < 1e-300)] = 1e-300
+    return P
