f4d7e0cca4b041aceebcb19b34522b7c46a5ea5c
DeiviGjyshi <deivi.gjyshi@gmail.com>
2023-10-28 09:11:01 -0700

    new

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
-"""availableShips method"""
+"""sentientPlanets method"""
 import requests as rq
 
 
-def availableShips(passengerCount):
-    """method that returns the list of ships
-    that can hold a given number of passengers"""
-    vehicles = []
+def sentientPlanets():
+    """Function returns the list of names of
+    the home planets of all sentient species"""
+    planets = []
     page = 1
     state = True
     while state:
-        url = "https://swapi-api.hbtn.io/api/starships/?page=" + str(page)
+        url = "https://swapi-api.hbtn.io/api/species/?page=" + str(page)
         r = rq.get(url)
         data = r.json()
         results = data['results']
-        for vehicle in results:
-            passenger = vehicle['passengers']
-            passenger = passenger.replace(',', "")
-            if passenger.isnumeric() and int(passenger) >= passengerCount:
-                vehicles.append(vehicle['name'])
+        for specie in results:
+            if specie['classification'] == 'sentient' or \
+               specie['designation'] == 'sentient':
+                homeworld = specie['homeworld']
+                if homeworld is not None:
+                    req = rq.get(specie['homeworld'])
+                    Data = req.json()
+                    planets.append(Data['name'])
         if data['next'] is None:
             state = False
         page += 1
-    return vehicles
+    return planets
